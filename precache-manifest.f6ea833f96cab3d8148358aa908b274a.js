cb49b06656a9a4ad1521afaa034babf2be29e1ea
freddy <freddy03h@gmail.com>
2019-05-09 17:37:42 +0200

    Updates

---
+++ precache-manifest.f6ea833f96cab3d8148358aa908b274a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a51cdd3f9b1f439c3b9c903eab3221b",
+    "url": "/egapro/index.html"
+  },
+  {
+    "revision": "41c002a2ef064321b3d1",
+    "url": "/egapro/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "6d91a755c0342c1ba262",
+    "url": "/egapro/static/css/main.abee4a3f.chunk.css"
+  },
+  {
+    "revision": "41c002a2ef064321b3d1",
+    "url": "/egapro/static/js/2.11fddbb8.chunk.js"
+  },
+  {
+    "revision": "6d91a755c0342c1ba262",
+    "url": "/egapro/static/js/main.65b49f54.chunk.js"
+  },
+  {
+    "revision": "69ebad1239518cd2ea63",
+    "url": "/egapro/static/js/runtime~main.04610f6f.js"
+  }
+]);
